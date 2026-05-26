78d7f549d386bd7f2b78c3d7bd954440d6aa525b
Henry Chen <hjc77@cornell.edu>
2026-05-26 13:19:50 -0400

    Observability: Clean up Oshi
    No longer need Oshi to send system metrics to Prometheus, since we can
    directly monitor system resource usage of kubernetes nodes.
    
    Keeping the commit sha, since it's still included in all of the metrics
    generated by Spring Actuator.

--- src/main/java/org/patinanetwork/codebloom/utilities/SystemMetricsConfig.java
+++ src/main/java/org/patinanetwork/codebloom/utilities/SystemMetricsConfig.java
@@ -7,37 +7,26 @@ import org.patinanetwork.codebloom.utilities.sha.CommitShaProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import oshi.SystemInfo;
-import oshi.hardware.GlobalMemory;
 
+/**
+ * Configuration for system metrics exposed to Prometheus through Spring Actuator.
+ * Prometheus ingests the metrics from Actuator through a Kubernetes object defined in the k8s-manifest.
+ *
+ * @see <a href= "https://github.com/Patina-Network/k8s-manifests/blob/main/base/production/codebloom/servicemonitor.yaml">Service Monitor</a>
+ */
 @Configuration
 @EnableConfigurationProperties(CommitShaProperties.class)
 public class SystemMetricsConfig {
 
-    @Bean
-    public SystemInfo systemInfo() {
-        return new SystemInfo();
-    }
 
+    /**
+     * Add commit sha to metrics so that the deployed version of the code being run on the instance is known.
+     */
     @Bean
     public MeterBinder applicationInfoMetrics(CommitShaProperties commitShaProperties) {
         return registry -> {
             var tags = Tags.of(Tag.of("sha", commitShaProperties.getSha()));
             registry.gauge("application.info", tags, 1, n -> 1.0);
-        };
-    }
-
-    @Bean
-    public MeterBinder systemMemoryMetrics(SystemInfo systemInfo) {
-        return registry -> {
-            GlobalMemory memory = systemInfo.getHardware().getMemory();
-            registry.gauge("system.info.memory.total", memory, GlobalMemory::getTotal);
-            registry.gauge("system.info.memory.available", memory, GlobalMemory::getAvailable);
-            registry.gauge("system.info.memory.used", memory, m -> m.getTotal() - m.getAvailable());
-            registry.gauge(
-                    "system.info.memory.usage",
-                    memory,
-                    m -> (double) (m.getTotal() - m.getAvailable()) / m.getTotal() * 100);
         };
     }
 }
