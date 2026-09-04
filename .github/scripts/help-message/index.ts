fd1964da82e583f3e75e556a3487aeace3be637c
Tahmid Ahmed <tahmid@tahmid.io>
2026-09-04 11:28:17 -0400

    TAN-58: Clean up deployments
    - Condense build-image & deployments script into one, callable by both composite workflows
    - Clean up any remnants of GPG_PASSPHRASE/GPG_PRIVATE_KEY
    - moved to new digitalocean PAT that targets `patinanetwork` instead of
      `tahminator`
    - do some cleanup in regards to the help cmd (we no longer have any
      valid ones)

--- .github/scripts/help-message/index.ts
+++ .github/scripts/help-message/index.ts
@@ -1,4 +1,4 @@
-import { sendMessage } from "utils/send-message";
+import { GitHubClient } from "@tahminator/pipeline";
 import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
 
@@ -12,27 +12,57 @@ const { prId } = await yargs(hideBin(process.argv))
   .parse();
 
 export async function main() {
-  await sendMessage(
+  const { githubAppAppId, githubAppInstallationId, githubAppPrivateKey } =
+    parseCiEnv(process.env);
+
+  const githubClient = await GitHubClient.createWithGithubAppToken({
+    appId: githubAppAppId,
+    installationId: githubAppInstallationId,
+    privateKey: githubAppPrivateKey,
+  });
+
+  await githubClient.sendPrMessage({
     prId,
-    `
+    owner: "Patina-Network",
+    repository: "codebloom",
+    message: `
 ### Available PR Commands
 
-- \`/ai\` - Triggers all AI review commands at once
-- \`/review\` - AI review of the PR changes
-- \`/describe\` - AI-powered description of the PR
-- \`/improve\` - AI-powered suggestions
-- \`/deploy\` - Deploy to staging
-
-See: https://github.com/tahminator/codebloom/wiki/CI-Commands
+There are currently none available at this time.
 `.trim(),
-  );
+  });
 }
 
-main()
-  .then(() => {
-    process.exit();
-  })
-  .catch((e) => {
-    console.error(e);
-    process.exit(1);
-  });
+function parseCiEnv(ciEnv: Record<string, string | undefined>) {
+  const githubAppAppId = (() => {
+    const v = ciEnv["_GITHUB_APP_APP_ID"];
+    if (!v) {
+      throw new Error("Missing _GITHUB_APP_APP_ID from env");
+    }
+    return v;
+  })();
+
+  const githubAppInstallationId = (() => {
+    const v = ciEnv["_GITHUB_APP_INSTALLATION_ID"];
+    if (!v) {
+      throw new Error("Missing _GITHUB_APP_INSTALLATION_ID from env");
+    }
+    return v;
+  })();
+
+  const githubAppPrivateKey = (() => {
+    const v = ciEnv["_GITHUB_APP_PEM_CONTENT"];
+    if (!v) {
+      throw new Error("Missing _GITHUB_APP_PEM_CONTENT from env");
+    }
+    return v;
+  })();
+
+  return {
+    githubAppAppId,
+    githubAppInstallationId,
+    githubAppPrivateKey,
+  };
+}
+
+main();
