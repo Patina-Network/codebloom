358d3c534596644445eba8e3894a03a3b2a8fa3b
Tahmid Ahmed <tahmid@tahmid.io>
2026-09-05 17:36:57 -0700

    TAN-58: Clean up deployments (#964)
    - Condense build-image & deployments script into one, callable by both composite workflows
    - Clean up any remnants of GPG_PASSPHRASE/GPG_PRIVATE_KEY
    - moved to new digitalocean PAT that targets `patinanetwork` instead of
      `tahminator`
    - do some cleanup in regards to the help cmd (we no longer have any
      valid ones)

--- .github/scripts/redeploy/internal/coolify/index.ts
+++
@@ -1,8 +0,0 @@
-import { Coolify } from "coolify/src";
-
-export async function initClient(bearerAuth: string, serverURL: string) {
-  return new Coolify({
-    bearerAuth,
-    serverURL,
-  });
-}
